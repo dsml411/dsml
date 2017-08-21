7425edd4f0536b2f55a7ba9e28aeabfd5724e3f8
dsml411 <30291532+dsml411@users.noreply.github.com>
2017-08-21 11:21:51 +0800

    8/21

--- oritatami.js
+++ oritatami.js
@@ -17,3 +17,16 @@ function expand(URI,SUB,EXP){
     return true;
   }
 }
+
+var id,i ;		
+function visible(n) {
+	id=[document.getElementById('m0'),document.getElementById('m1'),document.getElementById('m2'),document.getElementById('m3'),document.getElementById('m4'),document.getElementById('m5'),document.getElementById('m6'),document.getElementById('m7'),document.getElementById('m8'),document.getElementById('m9')] ;
+	for ( i = 0; i < id.length; i++) {
+		if (i==n) {
+			id[i].style.display='block' ;
+		}
+		else {
+			id[i].style.display='none' ;
+		}
+	}
+}
