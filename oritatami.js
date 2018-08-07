1534e5a0528a098fa604638f6f7eccff20836ab2
dsml411 <30291532+dsml411@users.noreply.github.com>
2018-08-07 18:44:45 +0800

    Update oritatami.js

--- oritatami.js
+++ oritatami.js
@@ -17,17 +17,3 @@ function expand(URI,SUB,EXP){
     return true;
   }
 }
-
-var id,id2,i ;		
-function visible(n) {
-	id='m'+n	
-	for ( i = 0; i < 30; i++) {    //預設最多30個人，成員太多就把i<30的30增加吧
-		id2='m'+i
-		if (i==n) {
-			document.getElementById(id).style.display='block' ;
-		}
-		else {
-			document.getElementById(id2).style.display='none' ;
-		}
-	}
-}
